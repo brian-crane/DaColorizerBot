ec580d6b32139e37edc2430f159a887b810d6bed
Brian <briancrane@gmail.com>
2020-06-20 17:56:26 -0700

    Added new Reddit rate limit exception handling and redirect comments

--- RedditBot.py
+++ RedditBot.py
@@ -9,6 +9,7 @@ from PIL import Image
 
 
 import praw
+from praw.exceptions import RedditAPIException
 
 import Tools
 
@@ -39,20 +40,54 @@ for submission in subreddit.hot(limit=1):
             img.save('temp.jpeg')
         #Colorize!
         cmd = "python Colorization.py --input temp.jpeg --output " + filename
+        start = time.time()
         os.system(cmd)
-
+        start = time.time() - start
         #image is now saved at images/out/filename
         #submission.reply("This is a test of the new colorization Bot!")
 
         #Post the colorized image to my subreddit
         mySubreddit = reddit.subreddit("DaColorizerBot")
-        title = "Colorized: " + submission.title + " - u/" + submission.author.name
+        title = "Colorized: " + submission.title
         mySubmission = mySubreddit.submit_image(title, filename)
         #Post my comment linking to original B&W image
         print(filename + " was submitted as: " + title)
 
+        comment = "It took " + str(start)[0:4] + " seconds to colorize this image using [Deep Learning](https://cv-tricks.com/opencv/deep-learning-image-colorization/)!\n\n[Original Image]("+submission.url+")\n\n[Source](" \
+                  + submission.shortlink + "): *" + submission.title +"*"
+        mySubmission.reply(comment)
+        print("I left this comment on my subreddit's post:\n\t" + comment)
+
+        #Now leave comment on the orginal post, redirecting to my new post's reddit image url
+        redirectComment = "Hello! I'm a bot and I colorized this image using [Deep Learning](https://cv-tricks.com/opencv/deep-learning-image-colorization/)." \
+        + "\n\n[Colorized Image](" + mySubmission.url +")\n\nIt took me " + str(start)[0:4] + " seconds to make this image 😰"
+        try:
+            submission.reply(redirectComment)
+        except RedditAPIException as e:
+            if "doing that too much" in str(e):
+                seconds = 0
+                if "second" in str(e):
+                    seconds = str(e)[0: str(e).index(" second")]
+                    seconds += int(seconds[seconds.rindex(" "):])
+                    print("asdasd")
+                if "minute" in str(e):
+                    tmp = str(e)[0: str(e).index(" minute")]
+                    seconds += 60 * int(tmp[tmp.rindex(" "):])
+            #Add a grace period just to be nice to reddit's servers
+            seconds += 30
+            print("We have a RATE LIMIT message from reddit: \n\t" + str(e))
+            print("We need to wait for " + str(seconds-30) + " more seconds!")
+            while seconds > 5 + seconds/2:
+                time.sleep(6 + seconds/2)
+                seconds -= 6 + seconds/2
+                print(seconds + " left...")
+            print("Done Waiting!")
+            #Resubmit comment on original post
+            submission.reply(redirectComment)
+        print("I left this redirect comment on the orginal B&W post:\n\t" + redirectComment)
+
         #wait for reddit timer to refresh
-        print("Sleeping for 30 seconds")
+        print("Sleeping for 30 seconds - you completed one FULL POST cycle")
         time.sleep(30)
     else:
         print("WE alredy proceseed " + filename + "......")
